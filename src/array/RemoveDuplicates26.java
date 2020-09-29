9fa0e48835c561d7cf51bfe64c95556e69dea49e
YoungTr <tangyan1004@gmail.com>
2020-09-29 11:35:42 +0800

    remove duplicates

---
+++ src/array/RemoveDuplicates26.java
@@ -0,0 +1,16 @@
+package array;
+
+public class RemoveDuplicates26 {
+    public int removeDuplicates(int[] nums) {
+        int j = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[j] != nums[i]) {
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+
+        return j + 1;
+    }
+}
