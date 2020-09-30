293ce1ec01386e021ea8885b8359ccc81a2700d8
tangyan <tangyan1004@gmail.com>
2020-09-30 09:53:19 +0800

    remove duplicates 80

---
+++ src/array/RemoveDuplicates80.java
@@ -0,0 +1,24 @@
+package array;
+
+public class RemoveDuplicates80 {
+    public int removeDuplicates(int[] nums) {
+        int j = 0;
+        int count = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[j]) {
+                count++;
+            } else {
+                count = 1;
+            }
+            if (count <= 2) {
+                j++;
+                if (j != i) {
+                    nums[j] = nums[i];
+                }
+            }
+        }
+
+        return j + 1;
+    }
+}
