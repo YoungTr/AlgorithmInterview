bbc8a50a35c3039adbc4963348fd97fe5ee729b2
YoungTr <tangyan1004@gmail.com>
2020-11-26 20:04:07 +0800

    83

---
+++ src/link/DeleteDuplicates83.java
@@ -0,0 +1,16 @@
+package link;
+
+public class DeleteDuplicates83 {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode cur = head;
+        while (cur != null && cur.next != null) {
+            if (cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+}
